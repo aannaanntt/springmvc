cd8048b729b51d568c6f1878d0a660c2686f1978
aannaanntt <anantmishra2307@gmail.com>
2021-03-24 23:55:57 -0700

    fourth commit

--- DemoMVC/target/classes/AddController.java
+++ DemoMVC/target/classes/AddController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,16 +12,16 @@ public class AddController {
 	
 	
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request,HttpServletResponse response) {
-		int i = Integer.parseInt(request.getParameter("t1"));
-		
-		int j = Integer.parseInt(request.getParameter("t2"));
+	public ModelAndView add(@RequestParam("t1") i,@RequestParam("t2")intj,HttpServletRequest request,HttpServletResponse response) {
+//		int i = Integer.parseInt(request.getParameter("t1"));
+//		
+//		int j = Integer.parseInt(request.getParameter("t2"));
 		
 		AddService as = new AddService();
 		int k = as.add(i,j);
 		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("display.jsp");
+		mv.setViewName("display");
 		mv.addObject("result",k)
 		
 		return mv;
